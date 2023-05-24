793f0f407dcaf91b00f67f8608598e3bd5878a44
aufaazz <aufa.azmrn@gmail.com>
2023-05-24 22:28:27 +0700

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar app color="#111111" dark flat class="px-8">
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="auto">
+          <v-btn text @click="scroll('home')">Home</v-btn>
+          <v-btn text @click="scroll('project')">Project</v-btn>
+          <v-btn text @click="scroll('about')">about</v-btn>
+          <v-btn text @click="scroll('contact')"> contact</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+   
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  methods: {
+    scroll(refName) {
+      const element = document.getElementById(refName);
+      element.scrollIntoView({ behavior: "smooth" });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,325 @@
+<template>
+  <v-app
+    id="home"
+    :style="{ background: $vuetify.theme.themes.light.background }"
+  >
+    <nav-bar />
+    <v-container fluid>
+      <v-row>
+        <v-col cols="6">
+          <v-img src="1.png" contain max-height="500"></v-img>
+        </v-col>
+        <v-col cols="6">
+          <h4 class="black--text text--darken-4 top">Hello, I'm Aufa Azmirania</h4>
+          <h1 class="black--text">Product Designer</h1>
+          <p class="grey--text">
+            As a product designer, I have a passion for creating user-centered and engaging experiences that solve real-world problems. With a background in graphic design and a deep understanding of user research and design thinking, I bring a unique perspective to the product development process.
+          </p>
+          <v-btn tile color="#F9B83B" dark> about me </v-btn>
+        </v-col>
+
+        <v-col cols="12" class="padd">
+          <div class="first" id="project">
+            <v-row>
+              <v-col cols="12">
+                <div class="child bgColor1">
+            
+                  <v-icon color="#F9B83B" x-large class="ml-3"
+                    >mdi-palette-swatch</v-icon
+                  >
+                  <h3 class="yellow--text text--darken-2 ml-3 mt-4">UI Design</h3>
+                  <p class="grey--text ml-3 mt-6">
+                    User Interface design : <br />websites, applications, software, etc.
+                  </p>
+                  <v-btn color="#F9B83B" dark text>
+                    know more
+                    <v-icon right>mdi-arrow-right</v-icon>
+                  </v-btn>
+                </div>
+                <div class="child bgColor2">
+                  <v-icon color="" x-large class="ml-3" dark>
+                    mdi-shopping
+                  </v-icon>
+                  <h3 class="white--text ml-3 mt-4">Product Design</h3>
+                  <p class="grey--text ml-3 mt-6">
+                    Product Design : <br />research, prototyping, others. 
+                  </p>
+                  <v-btn color="" dark text>
+                    know more
+                    <v-icon right>mdi-arrow-right</v-icon>
+                  </v-btn>
+                </div>
+
+                <div class="child bgColor1">
+                  <v-icon color="#F9B83B" x-large class="ml-3"
+                    >mdi-book-open-page-variant</v-icon
+                  >
+                  <h3 class="yellow--text text--darken-2 ml-3 mt-4">UX Design</h3>
+                  <p class="grey--text ml-3 mt-6">
+                    User Experience design : <br />research, information architecture, etc.
+                  </p>
+                  <v-btn color="#F9B83B" dark text>
+                    know more
+                    <v-icon right>mdi-arrow-right</v-icon>
+                  </v-btn>
+                </div>
+              </v-col>
+              <v-col cols="12" class="mt-10">
+                <div class="child1">
+                  <h1 class="black--text text--darken-4 mt-4 number" >2</h1>
+                  <h3 class="black--text mt-4">Years Experience</h3>
+                </div>
+                <div class="child2 mRight">
+                  <v-row>
+                    <v-col cols="12" class="childcol">
+                      <div class="child2 mButton padding bgColor1">
+                        <h1 class="black--text">10+</h1>
+                        <p class="grey--text">Clients</p>
+                      </div>
+                    </v-col>
+                    <v-col cols="12" class="childcol">
+                      <div class="child2 padding bgColor1">
+                        <h1 class="black--text">21+</h1>
+                        <p class="grey--text">Completed Projects</p>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </div>
+                <div class="child2">
+                  <v-row>
+                    <v-col cols="12" class="childcol">
+                      <div class="child2 mButton padding bgColor1">
+                        <h1 class="black--text">02</h1>
+                        <p class="grey--text">Years Experience</p>
+                      </div>
+                    </v-col>
+                    <v-col cols="12" class="childcol">
+                      <div class="child2 padding bgColor1">
+                        <h1 class="black--text">10+</h1>
+                        <p class="grey--text">Achievements</p>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </div>
+              </v-col>
+            </v-row>
+          </div>
+        </v-col>
+        <v-col md="3" offset-md="3" id="about">
+          <h4 class="black--text">FEATURED PROJECTS</h4>
+          <p class="grey--text">Here are the projects I have worked on</p>
+        </v-col>
+        <v-col md="3" class="text-end">
+          <v-btn tile color="#F9B83B"> View All </v-btn>
+        </v-col>
+        <v-col md="3" offset-md="3">
+          <v-card
+            outlined
+            tile
+            height="250px"
+            color="#1E1E1E"
+          >
+            <v-img src="3.png" contain max-height=""></v-img>
+          </v-card>
+        </v-col>
+        <v-col md="3">
+          <v-card
+            outlined
+            tile
+            height="250px"
+            color="#1E1E1E"
+          >
+            <v-img src="2.png" contain max-height=""></v-img>
+          </v-card>
+        </v-col>
+        <v-col md="3" offset-md="3">
+          <v-btn color="black" dark text class="ml-n4"> Dispace </v-btn
+          ><br />
+
+          <v-btn color="#F9B83B" dark text class="ml-n4">
+            know more
+            <v-icon right>mdi-arrow-right</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col md="3">
+          <v-btn color="black" dark text class="ml-n4"> Safha </v-btn><br />
+
+          <v-btn color="#F9B83B" dark text class="ml-n4">
+            know more
+            <v-icon right>mdi-arrow-right</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col md="3" offset-md="3">
+          <v-card
+            outlined
+            tile
+            height="250px"
+            color="#1E1E1E"
+          >
+            <v-img src="4.png" contain max-height=""></v-img>
+          </v-card>
+        </v-col>
+        <v-col md="3">
+          <v-card
+            outlined
+            tile
+            height="250px"
+            color="#1E1E1E"
+          >
+            <v-img src="5.png" contain max-height=""></v-img>
+          </v-card>
+        </v-col>
+        <v-col md="3" offset-md="3">
+          <v-btn color="black" dark text class="ml-n4"> Colab </v-btn
+          ><br />
+
+          <v-btn color="#F9B83B" dark text class="ml-n4">
+            know more
+            <v-icon right>mdi-arrow-right</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col md="3">
+          <v-btn color="black" dark text class="ml-n4"> Muslimate </v-btn><br />
+          <v-btn color="#F9B83B" dark text class="ml-n4">
+            know more
+            <v-icon right>mdi-arrow-right</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col cols="12" class="padd topInverse">
+          <div class="second">
+            <div class="secondchild1" id="contact">
+              <v-row>
+                <v-col cols="7">
+                  <h1 class="white--text">
+                    Let's work together on<br />your next project
+                  </h1>
+                </v-col>
+                <v-col cols="5">
+                  <v-btn tile color="#black" dark class="mt-16">
+                    Contact
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </div>
+          </div>
+          <v-toolbar class="topTolbar" color="#FFFFFF" light flat>
+            <div
+              style="
+                position: absolute;
+                margin-left: auto;
+                margin-right: auto;
+                left: 0;
+                right: 0;
+                text-align: center;"
+            >
+              <v-btn text>Home</v-btn>
+              <v-btn text>Project</v-btn>
+              <v-btn text>about</v-btn>
+              <v-btn text> contact</v-btn>
+            </div>
+          </v-toolbar>
+        </v-col>
+      </v-row>
+    </v-container>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import NavBar from "../components/NavBar.vue";
+import Footer from "../components/Footer.vue";
+
+export default {
+  name: "Home",
+  data: () => ({
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+  }),
+  components: {
+    NavBar,
+    Footer,
+  },
+};
+</script>
+<style scoped>
+.top {
+  margin-top: 180px;
+}
+.topInverse {
+  margin-top: -250px;
+}
+.topTolbar {
+  margin-top: 100px;
+  text-align: center;
+}
+.first {
+  width: 100%;
+  height: 610px;
+  background: linear-gradient(
+    to right,
+    #ffffff
+  );
+  text-align: center;
+  padding: 2rem 2rem;
+}
+.second {
+  width: 100%;
+  height: 400px;
+  background: #ffffff;
+  text-align: center;
+  padding: 2rem 2rem;
+}
+.secondchild1 {
+  display: inline-block;
+  background-color: #F9B83B;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  text-align: left;
+  margin-top: 250px;
+}
+.child {
+  display: inline-block;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  text-align: left;
+  margin-right: 8px;
+}
+.bgColor1 {
+  background-color: #f3f3f3;
+}
+.bgColor2 {
+  background-color: #111111;
+}
+
+.child1 {
+  display: inline-block;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  margin-right: 5px;
+  width: 240px;
+}
+.child2 {
+  display: inline-block;
+  width: 245px;
+  vertical-align: middle;
+}
+.mRight {
+  margin-right: 8px;
+}
+.mButton {
+  margin-bottom: 8px;
+}
+.padding {
+  padding: 8px 0;
+}
+
+.col-12.padd {
+  padding: 12px 0 !important;
+}
+.col-12.childcol {
+  padding: 0 !important;
+}
+h1.number {
+  font-size: 50px;
+  font-weight: bold;
+}
+</style>
